ac0c0f92363d79768c1d9c89564b7ad072672662
rakiyama <ryojiro25@gmail.com>
2021-12-10 20:33:42 +0900

    separate module

--- module00/ex00/ConvertImage/convert.go
+++ module00/ex00/ConvertImage/convert.go
@@ -1,5 +1,83 @@
 package convert
 
+import (
+	"bytes"
+	"fmt"
+	"image/jpeg"
+	"image/png"
+	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Convert struct {
-	dir string
+	Dir string
+}
+
+func (c Convert) JpgToPng() {
+	err := filepath.WalkDir(c.Dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if d.Type().IsRegular() {
+			//fmt.Print("file!: ")
+			jpg_to_png(path)
+		}
+		//fmt.Printf("visited file or dir: %q\n", path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("error walking the path: %v\n", err)
+		return
+	}
+}
+
+func jpg_to_png(srcFileName string) {
+	//fmt.Println("srcFileName: ", srcFileName)
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer srcFile.Close()
+	srcBytes, err := ioutil.ReadAll(srcFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dstBytes, err := toPng(srcBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	dstFile, err := os.Create(strings.TrimSuffix(srcFileName, ".jpg") + ".png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer dstFile.Close()
+	_, er := dstFile.Write(dstBytes)
+	if er != nil {
+		fmt.Println(err)
+	}
+}
+
+func toPng(srcBytes []byte) ([]byte, error) {
+	contentType := http.DetectContentType(srcBytes)
+
+	switch contentType {
+	case "image/png":
+	case "image/jpeg":
+		img, err := jpeg.Decode(bytes.NewReader(srcBytes))
+		if err != nil {
+			return nil, fmt.Errorf("%v: unable to decode jpeg", err)
+		}
+		buf := new(bytes.Buffer)
+		if err := png.Encode(buf, img); err != nil {
+			return nil, fmt.Errorf("%v: unable to encode png", err)
+		}
+		return buf.Bytes(), nil
+	}
+	return nil, fmt.Errorf("unable to convert %#v to png", contentType)
 }
--- module00/ex00/main.go
+++ module00/ex00/main.go
@@ -4,16 +4,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"image/jpeg"
-	"image/png"
-	"io/fs"
-	"io/ioutil"
-	"net/http"
+	"github.com/ryojiroakiyama/convert"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -27,70 +20,6 @@ func main() {
 		panic("invalid argument")
 	}
 	dir := os.Args[1]
-	ConvertAll(dir)
-}
-
-func ConvertAll(dir string) {
-	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
-			return err
-		}
-		if d.Type().IsRegular() {
-			//fmt.Print("file!: ")
-			jpg_to_png(path)
-		}
-		//fmt.Printf("visited file or dir: %q\n", path)
-		return nil
-	})
-	if err != nil {
-		fmt.Printf("error walking the path: %v\n", err)
-		return
-	}
-}
-
-func jpg_to_png(srcFileName string) {
-	//fmt.Println("srcFileName: ", srcFileName)
-	srcFile, err := os.Open(srcFileName)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer srcFile.Close()
-	srcBytes, err := ioutil.ReadAll(srcFile)
-	if err != nil {
-		fmt.Println(err)
-	}
-	dstBytes, err := ToPng(srcBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	dstFile, err := os.Create(strings.TrimSuffix(srcFileName, ".jpg") + ".png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer dstFile.Close()
-	_, er := dstFile.Write(dstBytes)
-	if er != nil {
-		fmt.Println(err)
-	}
-}
-
-func ToPng(srcBytes []byte) ([]byte, error) {
-	contentType := http.DetectContentType(srcBytes)
-
-	switch contentType {
-	case "image/png":
-	case "image/jpeg":
-		img, err := jpeg.Decode(bytes.NewReader(srcBytes))
-		if err != nil {
-			return nil, fmt.Errorf("%v: unable to decode jpeg", err)
-		}
-		buf := new(bytes.Buffer)
-		if err := png.Encode(buf, img); err != nil {
-			return nil, fmt.Errorf("%v: unable to encode png", err)
-		}
-		return buf.Bytes(), nil
-	}
-	return nil, fmt.Errorf("unable to convert %#v to png", contentType)
+	c := convert.Convert{Dir: dir}
+	c.JpgToPng()
 }
