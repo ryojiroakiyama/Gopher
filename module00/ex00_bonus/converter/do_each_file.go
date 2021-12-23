3c63dfd34248221c578455043232ba719933d6fb
rakiyama <ryojiro25@gmail.com>
2021-12-23 17:33:48 +0900

    separate file

--- module00/ex00_bonus/converter/do_each_file.go
+++ module00/ex00_bonus/converter/do_each_file.go
@@ -6,11 +6,6 @@ package converter
 
 import (
 	"fmt"
-	"image"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
-	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -49,48 +44,6 @@ func getImages(Extension string) (images, error) {
 	default:
 		return nil, fmt.Errorf("invalid argment: %v", Extension)
 	}
-}
-
-type imageJpg struct{}
-
-func (i imageJpg) decode(r io.Reader) (image.Image, error) {
-	return jpeg.Decode(r)
-}
-
-func (i imageJpg) encode(w io.Writer, m image.Image) error {
-	return jpeg.Encode(w, m, nil)
-}
-
-type imagePng struct{}
-
-func (i imagePng) decode(r io.Reader) (image.Image, error) {
-	return png.Decode(r)
-}
-
-func (i imagePng) encode(w io.Writer, m image.Image) error {
-	return png.Encode(w, m)
-}
-
-type imageGif struct{}
-
-func (i imageGif) decode(r io.Reader) (image.Image, error) {
-	return gif.Decode(r)
-}
-
-func (i imageGif) encode(w io.Writer, m image.Image) error {
-	return gif.Encode(w, m, nil)
-}
-
-type conversion struct {
-	srcExtension string
-	dstExtension string
-	encoder      images
-	decoder      images
-}
-
-type images interface {
-	decode(r io.Reader) (image.Image, error)
-	encode(w io.Writer, m image.Image) error
 }
 
 /*
