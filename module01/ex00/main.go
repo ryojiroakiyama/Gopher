8bc1201446a72aa301e22da8518be44deedd1615
rakiyama <ryojiro25@gmail.com>
2022-01-07 13:17:05 +0900

    make 01/ex00

--- module01/ex00/main.go
+++ module01/ex00/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"io"
+	"os"
 )
 
-func cat(in io.Reader, out io.Writer) {
-
+func cat(in io.Reader, out io.Writer) error {
+	buf := make([]byte, 101)
+	for {
+		n, err := in.Read(buf)
+		if n > 0 {
+			if _, err := out.Write(buf[:n]); err != nil {
+				return err
+			}
+		}
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
-
+	cat(os.Stdin, os.Stdout)
 }
