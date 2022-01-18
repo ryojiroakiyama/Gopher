a0b478ad69bf1e417b4d464d6fa1ac16371c20e8
rakiyama <ryojiro25@gmail.com>
2022-01-18 10:46:57 +0900

    add word slice main

--- module02/ex00/main.go
+++ module02/ex00/main.go
@@ -21,18 +21,22 @@ func nextLine(sc *bufio.Scanner, ch chan<- string) {
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	ch := make(chan string)
-	for {
-		word := "apple"
-		fmt.Printf("  %v\n", word)
+	words := [3]string{"apple", "water", "42tokyo"}
+	for idx := 0; true; idx++ {
+		if idx == 3 {
+			idx = 0
+		}
+		fmt.Printf("  %v\n", words[idx])
 		fmt.Printf("> ")
 		go nextLine(sc, ch)
 		select {
 		case get := <-ch:
-			if word == get {
+			if words[idx] == get {
+				fmt.Println("Congratulations!")
 				return
 			}
 		case <-time.After(5 * time.Second):
-			fmt.Println("timed out")
+			fmt.Println("\ntimed out (>_<;)")
 			return
 		}
 	}
