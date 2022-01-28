11559a29e3477ad380353b44c1e5712adfa5b544
rakiyama <ryojiro25@gmail.com>
2022-01-28 12:50:33 +0900

    add comment

--- module02/ex01/main.go
+++ module02/ex01/main.go
@@ -25,15 +25,6 @@ const (
 	RESET          = "\033[0m"
 )
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprintf(w, "Hello, akiyama in the house")
-//}
-
-//func main() {
-//	http.HandleFunc("/", handler)
-//	http.ListenAndServe(":8080", nil)
-//}
-
 func printError(err error) {
 	fmt.Fprintln(os.Stderr, "error:", err)
 }
@@ -62,6 +53,8 @@ func rangeStr(start int, end int) string {
 }
 
 func DownloadFile(filepath string, url string) (err error) {
+
+	// get all length by reponse header
 	resp_h, _ := http.Head(url)
 	maps := resp_h.Header
 	length, _ := strconv.Atoi(maps["Content-Length"][0])
@@ -71,34 +64,36 @@ func DownloadFile(filepath string, url string) (err error) {
 	end2 := length - 1
 	fmt.Printf("range1:%v-%v, range2:%v-%v, length:%v\n", start1, end1, start2, end2, length)
 
+	// get first range response
 	range1 := rangeStr(start1, end1)
 	client1 := &http.Client{}
 	req1, _ := http.NewRequest("GET", url, nil)
 	req1.Header.Add("Range", range1)
-	//resp1, err := http.Get(url)
 	resp1, err := client1.Do(req1)
 	if err != nil {
 		return err
 	}
 	defer resp1.Body.Close()
 
+	// get last range response
 	range2 := rangeStr(start2, end2)
 	client2 := &http.Client{}
 	req2, _ := http.NewRequest("GET", url, nil)
 	req2.Header.Add("Range", range2)
-	//resp2, err := http.Get(url)
 	resp2, err := client2.Do(req2)
 	if err != nil {
 		return err
 	}
 	defer resp2.Body.Close()
 
+	// bind body
 	allbody := &bytes.Buffer{}
 	body_bytes, _ := io.ReadAll(resp1.Body)
 	allbody.Write(body_bytes)
 	body_bytes, _ = io.ReadAll(resp2.Body)
 	allbody.Write(body_bytes)
 
+	// write into outfile
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
