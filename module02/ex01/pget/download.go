76fe61d027ae3960d1405e32d7101a46929d149b
rakiyama <ryojiro25@gmail.com>
2022-02-07 18:42:12 +0900

    done by string ch

--- module02/ex01/pget/download.go
+++ module02/ex01/pget/download.go
@@ -1,6 +1,7 @@
 package pget
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -22,28 +23,18 @@ func Do(filepath string, url string) (err error) {
 	}
 	numDivide := NumDivideRange(sizeTotal)
 	sizeDivide := sizeTotal / int64(numDivide)
-	chStr := make([]chan string, numDivide)
-	tmpFileNames := make([]string, numDivide)
-	defer func() {
-		for _, t := range tmpFileNames {
-			if t != "" {
-				fmt.Println("\ntmpfile:", t)
-				os.Remove(t)
-			}
-		}
-	}()
+	ch := make([]chan string, numDivide)
+	ctx, cancel := context.WithCancel(context.Background())
 	for i := 0; i < numDivide; i++ {
-		chStr[i] = make(chan string)
 		wg.Add(1)
-		go download(i, numDivide, sizeDivide, sizeTotal, url, chStr[i])
+		ch[i] = download(ctx, i, numDivide, sizeDivide, sizeTotal, url)
 	}
-	fmt.Println("\n---------------------")
 	dstFile, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer dstFile.Close()
-	for _, ch := range chStr {
+	for _, ch := range ch {
 		name := <-ch
 		srcfile, err := os.Open(name)
 		if err != nil {
@@ -54,40 +45,57 @@ func Do(filepath string, url string) (err error) {
 			return err
 		}
 	}
+	cancel()
 	wg.Wait()
 	return err
 }
 
-func download(index int, numDivide int, sizeDivide int64, sizeTotal int64, url string, chStr chan<- string) {
-	defer wg.Done()
-	minRange := sizeDivide * int64(index)
-	maxRange := sizeDivide * int64(index+1)
-	if index == numDivide-1 {
-		maxRange += sizeTotal - maxRange
-	}
-	fmt.Printf("index=%v, min=%v, max=%v\n", index, minRange, maxRange-1)
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Add("Range", RangeValue(minRange, maxRange-1))
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	tmpfile, err := os.CreateTemp("", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%v ", tmpfile.Name())
-	_, err = io.Copy(tmpfile, resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err = tmpfile.Close(); err != nil {
-		log.Fatal(err)
-	}
-	chStr <- tmpfile.Name()
+func download(ctx context.Context, index int, numDivide int, sizeDivide int64, sizeTotal int64, url string) chan string {
+	fileCh := make(chan string)
+	go func() {
+		defer wg.Done()
+		minRange := sizeDivide * int64(index)
+		maxRange := sizeDivide * int64(index+1)
+		if index == numDivide-1 {
+			maxRange += sizeTotal - maxRange
+		}
+		fmt.Printf("index=%v, min=%v, max=%v\n", index, minRange, maxRange-1)
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Add("Range", RangeValue(minRange, maxRange-1))
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		tmpfile, err := os.CreateTemp("", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			fmt.Println("\nrm %v", tmpfile.Name())
+			os.Remove(tmpfile.Name())
+		}()
+		fmt.Printf("%v ", tmpfile.Name())
+		_, err = io.Copy(tmpfile, resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err = tmpfile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	LOOP:
+		for {
+			select {
+			case <-ctx.Done():
+				break LOOP
+			case fileCh <- tmpfile.Name():
+			}
+		}
+		close(fileCh)
+	}()
+	return fileCh
 }
