87fed55cc1a4f9b58859b4953f3d66f00ff133ad
rakiyama <ryojiro25@gmail.com>
2022-02-09 21:59:28 +0900

    add file package

--- module02/ex01/file/file.go
+++ module02/ex01/file/file.go
@@ -3,6 +3,7 @@ package file
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -50,4 +51,34 @@ func BindFiles(srcNames []string, dstName string) (err error) {
 		}
 	}
 	return err
+}
+
+func FileBytes(fileName string) ([]byte, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("fail to open: %v", err)
+	}
+	defer file.Close()
+	srcBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("fail to read: %v", err)
+	}
+	return srcBytes, nil
+}
+
+func ToFile(fileName string, contents []byte) (err error) {
+	dstFile, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("fail to create: %v", err)
+	}
+	defer func() {
+		if cerr := dstFile.Close(); cerr != nil {
+			err = fmt.Errorf("fail to close: %v", cerr)
+		}
+	}()
+	_, werr := dstFile.Write(contents)
+	if werr != nil {
+		return fmt.Errorf("fail to write: %v", werr)
+	}
+	return
 }
