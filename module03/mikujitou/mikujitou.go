c706fb1ba461cad75d106844f6a305d8cdab724c
rakiyama <ryojiro25@gmail.com>
2022-02-24 18:25:43 +0900

    mod

--- module03/mikujitou/mikujitou.go
+++ module03/mikujitou/mikujitou.go
@@ -3,12 +3,13 @@ package mikujitou
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
-var fortune_message = []string{
+var fortuneContents = []string{
 	"Dai-kichi",
 	"Kichi",
 	"Chuu-kichi",
@@ -19,25 +20,37 @@ var fortune_message = []string{
 }
 
 type Omikuji struct {
-	Fortune string `json:"fortune-omikuji"`
+	Fortune string `json:"Fortune: "`
 }
 
+//time型はafterなど使って比較できる
+//parse()で日付から時間を作って, 比較
+//現在日付はformatで一旦取得
 func Get(w http.ResponseWriter, _ *http.Request) {
 	t := time.Now()
-	rand.Seed(t.Unix())
 	layout := "01-02"
-	start, _ := time.Parse(layout, "01-01")
-	now, _ := time.Parse(layout, t.Format(layout))
-	end, _ := time.Parse(layout, "01-03")
-	w.Write([]byte(fmt.Sprintf("s:%v, n:%v, e:%v\n", start.String(), now.String(), end.String())))
-	var fortune_index int
-	// !(start > now) && !(now > end)
-	if !start.After(now) && !now.After(end) {
-		fortune_index = 0
-	} else {
-		fortune_index = rand.Intn(len(fortune_message))
+	start, err := time.Parse(layout, "01-01")
+	if err != nil {
+		log.Fatal(err)
 	}
-	o := Omikuji{Fortune: fortune_message[fortune_index]}
+	now, err := time.Parse(layout, t.Format(layout))
+	if err != nil {
+		log.Fatal(err)
+	}
+	end, err := time.Parse(layout, "01-03")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(fmt.Sprintf("s:%v, n:%v, e:%v\n", start.String(), now.String(), end.String())))
+	var fortuneIndex int
+	// !(start > now) && !(now > end) -> (start <= now) && (now <= end)
+	if !start.After(now) && !now.After(end) {
+		fortuneIndex = 0
+	} else {
+		rand.Seed(t.Unix())
+		fortuneIndex = rand.Intn(len(fortuneContents))
+	}
+	o := Omikuji{Fortune: fortuneContents[fortuneIndex]}
 	j, _ := json.Marshal(o)
 	w.Write(j)
 }
