50cc3a8d6661ee6d07cc454b8f96f0baa49deca8
rakiyama <ryojiro25@gmail.com>
2022-02-24 17:57:14 +0900

    add time comparison

--- module03/mikujitou/mikujitou.go
+++ module03/mikujitou/mikujitou.go
@@ -2,6 +2,7 @@ package mikujitou
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"time"
@@ -22,8 +23,21 @@ type Omikuji struct {
 }
 
 func Get(w http.ResponseWriter, _ *http.Request) {
-	rand.Seed(time.Now().Unix())
-	o := Omikuji{Fortune: fortune_message[rand.Intn(len(fortune_message))]}
+	t := time.Now()
+	rand.Seed(t.Unix())
+	layout := "01-02"
+	start, _ := time.Parse(layout, "01-01")
+	now, _ := time.Parse(layout, t.Format(layout))
+	end, _ := time.Parse(layout, "01-03")
+	w.Write([]byte(fmt.Sprintf("s:%v, n:%v, e:%v\n", start.String(), now.String(), end.String())))
+	var fortune_index int
+	// !(start > now) && !(now > end)
+	if !start.After(now) && !now.After(end) {
+		fortune_index = 0
+	} else {
+		fortune_index = rand.Intn(len(fortune_message))
+	}
+	o := Omikuji{Fortune: fortune_message[fortune_index]}
 	j, _ := json.Marshal(o)
 	w.Write(j)
 }
